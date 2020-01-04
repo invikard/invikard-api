a9b00a7c006689081eeee3c34fa21c06d154222e
hynra <hynra@yahoo.com>
2020-01-04 23:34:25 +0700

    poc: check load profile

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as Crawler from 'instagram-public-crawler';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  static async loadProfile():  Promise<any>{
+    try {
+      return await Crawler.lite("permanahendra", { raw: false });
+    }catch (e) {
+      throw new BadRequestException('Profile not found');
+    }
   }
+
+  static async getProfile(): Promise<any> {
+    return await AppService.loadProfile();
+  }
+
+
 }
