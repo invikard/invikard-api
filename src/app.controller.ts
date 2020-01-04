71fddbcc212b18f3df0ea8f564753ff337f4bb94
hynra <hynra@yahoo.com>
2020-01-04 23:45:47 +0700

    query params as `dto`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AppDto } from './app.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getProfile(@Query('account') account): Promise<any> {
-    return AppService.getProfile(account);
+  async getProfile(@Query() query: AppDto): Promise<any> {
+    return AppService.getProfile(query.account);
   }
 }
