1532ce776e54470248c79ad46b18b5fe9ea294f8
hynra <hynra@yahoo.com>
2020-01-04 23:39:36 +0700

    account as `query-params`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getProfile(): Promise<any> {
-    return AppService.getProfile();
+  async getProfile(@Query('account') account): Promise<any> {
+    return AppService.getProfile(account);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -4,16 +4,16 @@ import * as Crawler from 'instagram-public-crawler';
 @Injectable()
 export class AppService {
 
-  static async loadProfile():  Promise<any>{
+  static async loadProfile(account: String):  Promise<any>{
     try {
-      return await Crawler.lite("permanahendra", { raw: false });
+      return await Crawler.lite(account, { raw: false });
     }catch (e) {
       throw new BadRequestException('Profile not found');
     }
   }
 
-  static async getProfile(): Promise<any> {
-    return await AppService.loadProfile();
+  static async getProfile(account: String): Promise<any> {
+    return await AppService.loadProfile(account);
   }
 
 
